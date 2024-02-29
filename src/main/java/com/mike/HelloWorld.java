cf118ed68936310e9ea0f991cb43c0723e40d0b3
mars2work <mars2workmail@gmail.com>
2024-02-29 18:26:05 +0800

    initial commit

---
+++ src/main/java/com/mike/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.mike;
+
+public class HelloWorld {
+	
+	public void sayHello() {
+		System.out.print("Hello World");
+	}
+
+}
---
+++ src/test/java/com/mike/HelloWorldTest.java
@@ -0,0 +1,40 @@
+package com.mike;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+	
+	private final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+
+	@BeforeEach
+	public void setUp() {
+		System.setOut(new PrintStream(outStream));
+	}
+
+	@Test
+	public void testSayHello2() throws IOException {
+		HelloWorld hw = new HelloWorld();
+		hw.sayHello();
+
+		outStream.flush();
+        String printedOutPut = new String(outStream.toByteArray());
+		String[] outputData = printedOutPut.split(System.getProperty("line.separator"));
+
+
+		assertEquals("Hello World", outputData[0]);
+	}
+
+	@AfterEach
+	public void cleanUp(){
+		System.setOut(null);
+	}
+
+}
